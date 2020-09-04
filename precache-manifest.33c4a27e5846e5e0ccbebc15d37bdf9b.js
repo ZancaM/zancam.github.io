f041108572727c64ae87a75537651c8ffcb17885
Matteo <matteo@swappie.com>
2020-09-04 16:43:51 +0300

    Updates

---
+++ precache-manifest.33c4a27e5846e5e0ccbebc15d37bdf9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26096a8ee8956fa71b7bfaf4679e0d7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91900ee08b4c7fa3d8e1",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ce571bc2cca99456e5d0",
+    "url": "/static/js/2.82d857d4.chunk.js"
+  },
+  {
+    "revision": "91900ee08b4c7fa3d8e1",
+    "url": "/static/js/main.5cb2b9e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
